41cdb5504257b92ed80865fb31095396bef00937
97joep <jjnplnt.freelance@gmail.com>
2025-07-30 10:26:17 +0100

    Update: added link arrows to contact page

--- js/contact.js
+++ js/contact.js
@@ -6,8 +6,23 @@ function renderContact(contact) {
         const contactLinksContainer = document.querySelector('.contact-content--links');
         if (contactLinksContainer) {
             contact.forEach((item) => {
-                contactLinksContainer.innerHTML += `<a href="${item.link}" target="_blank" class="capitalise">${item.title}</a>`;
+                contactLinksContainer.innerHTML += `<a href="${item.link}" target="_blank" class="contact-content--link capitalise">${item.title}</a>`;
             });
         }
     }
+
+    fetch('components/link-arrow.html')
+    .then(response => {
+        if (!response.ok) throw new Error('Failed to load icon');
+        return response.text();
+    })
+    .then(svgHTML => {
+        const links = document.querySelectorAll('.contact-content--link');
+        links?.forEach((link) => {
+            link.innerHTML += svgHTML;
+        });
+    })
+    .catch(error => {
+        console.error('Error loading link icon:', error);
+    });
 }
